37baad5e8dd4b31b50d276410d318d84386bc59c
Kyle Werts <kwerts707@yahoo.com>
2018-02-23 15:16:21 -0800

    Add files via upload

--- kyleW.cpp
+++ kyleW.cpp
@@ -5,30 +5,3 @@
 #include "Ammo.h"
 #include <stdio.h>
 
-int ActiveAmmo::ammoCount = 0;
-
-ActiveAmmo::ActiveAmmo()
-{
-	next = 0;
-	prev = 0;
-	ammoCount++;
-}
-
-ActiveAmmo::~ActiveAmmo()
-{
-	ammoCount--;
-}
-
-void ActiveAmmo::init(float p[3], float v[3], float d)
-{
-	pos[0] = p[0];
-	pos[1] = p[1];
-	pos[2] = p[2];
-	vel[0] = v[0];
-	vel[1] = v[1];
-	vel[2] = v[2];
-
-	damage = d;
-	next = 0;
-	prev = 0;
-}
