1de91fe96771e4de593210237a3d1224287b8c81
Kyle Werts <kwerts707@yahoo.com>
2018-02-24 17:01:29 -0800

    Update kyleW.cpp

--- kyleW.cpp
+++ kyleW.cpp
@@ -3,11 +3,16 @@
  * Flu Fighters Power up and ammo file 
  */ 
 #include "Ammo.h"
-
 using namespace std;
 
 void displayText()
 {
-	cout << "kyleW standing by!" << endl;
+	Rect r;
+	int yres = 900;
+	glClear(GL_COLOR_BUFFER_BIT);
 
+	r.bot = yres - 100;
+	r.left = 10;
+	r.center = 0;
+	ggprint8b(&r, 16, 0xFF00FF00, "kwerts");
 }
