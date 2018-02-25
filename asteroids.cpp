b333d96d7d4dcb42b33b1f5d92e1179d429ee92f
Kyle Werts <kwerts707@yahoo.com>
2018-02-24 17:02:23 -0800

    Update asteroids.cpp

--- asteroids.cpp
+++ asteroids.cpp
@@ -853,6 +853,7 @@ void render()
 	ggprint8b(&r, 16, 0x00ffff00, "n bullets: %i", g.nbullets);
 	ggprint8b(&r, 16, 0x00ffff00, "n asteroids: %i", g.nasteroids);
 	ggprint8b(&r, 16, 0x00ffff00, "n asteroids destroyed: ");
+	displayText();
 	//
 	//-------------
 	//Draw the ship
