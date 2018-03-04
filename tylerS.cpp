9eeca1de4394f0da0682c5c8ac2d39d920ad9ef7
Tyler Sharp <t.d.sharp@outlook.com>
2018-03-04 12:26:09 -0800

    Committing exports

--- tylerS.cpp
+++ tylerS.cpp
@@ -1,40 +1,114 @@
-#include <iostream>
 using namespace std;
-#include <stdio.h>
-#include <stdlib.h>
-#include <cstdlib>
 #include "fonts.h"
-#include "log.h"
-#include <ctime>
-#include <cstring>
-#include <cmath>
-#include <X11/Xlib.h>
-#include <X11/keysym.h>
 #include <GL/glx.h>
-#include <unistd.h>
 
-void StartMenu()
+void startMenu(int xres, int yres, int TitleScreenTexture)
 {
-    /*GLuint TitleScreenTexture;
-
-    Image *TitleScreenImage = NULL;
-
     glBindTexture(GL_TEXTURE_2D, TitleScreenTexture);
     glBegin(GL_QUADS);
-        glTexCoord2f(1.0f, 0.0f); glVertex2f( 600.0f, 900.0f);
-        glTexCoord2f(1.0f, 1.0f); glVertex2f( 600.0f, 0.0f);
-        glTexCoord2f(0.0f, 1.0f); glVertex2f( 0.0f,   0.0f);
-        glTexCoord2f(0.0f, 0.0f); glVertex2f( 0.0f,   9000.0f);
+        glTexCoord2f(1.0f, 0.0f); glVertex2f( xres, yres);
+        glTexCoord2f(1.0f, 1.0f); glVertex2f( xres, 0.0f);
+        glTexCoord2f(0.0f, 1.0f); glVertex2f( 0.0f, 0.0f);
+        glTexCoord2f(0.0f, 0.0f); glVertex2f( 0.0f, yres);
     glEnd();
     glBindTexture(GL_TEXTURE_2D, 0);
     glPopMatrix();
-    
-    glClearColor(0.0, 0.0, 0.0, 1.0);
+}
+void drawOverlay(int xres, int yres, int lives, int shipTexture)
+{
+    /*glPushMatrix();
+    glBindTexture(GL_TEXTURE_2D, overlayTexture);
+    glBegin(GL_QUADS);
+        glTexCoord2f(1.0f, 0.0f); glVertex2f( xres, 200);
+        glTexCoord2f(1.0f, 1.0f); glVertex2f( xres, 0.0f);
+        glTexCoord2f(0.0f, 1.0f); glVertex2f( 0.0f, 0.0f);
+        glTexCoord2f(0.0f, 0.0f); glVertex2f( 0.0f, 200);
+    glEnd();
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glPopMatrix();
+*/
+    Rect r;
+    r.bot = 20;
+    r.left = 20;
+    r.center = 0;
+    ggprint16(&r, 16, 0xFB6AD0, "LIVES");
+    for (int i = 0; i < lives; i++) {
+        glColor3f(1.0f, 1.0f, 1.0f);
+        glPushMatrix();
+        glTranslatef(70*(i+2), 30, 0.0f);
+        glBindTexture(GL_TEXTURE_2D, shipTexture);
+        glBegin(GL_QUADS);
+            glTexCoord2f(1.0f, 0.0f); glVertex2f( 10.0f,  10.0f);
+            glTexCoord2f(1.0f, 1.0f); glVertex2f( 10.0f, -10.0f);
+            glTexCoord2f(0.0f, 1.0f); glVertex2f(-10.0f, -10.0f);
+            glTexCoord2f(0.0f, 0.0f); glVertex2f(-10.0f,  10.0f);
+        glEnd();
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glPopMatrix();
+    }
+}
 
-    Rect s;
-    s.bot = 450;
-    s.left = 280;
-    s.center = 0;
-    ggprint16(&s, 16, 0x00ff0000, "FLU FIGHTERS");
-    */
+void drawShip(float posA, float posB, float posC, int shipTexture)
+{
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glPushMatrix();
+    glTranslatef(posA, posB, posC);
+    glBindTexture(GL_TEXTURE_2D, shipTexture);
+    //glRotatef(g.ship.angle, 0.0f, 0.0f, 1.0f);
+    glBegin(GL_QUADS);
+        glTexCoord2f(1.0f, 0.0f); glVertex2f( 30.0f,  30.0f);
+        glTexCoord2f(1.0f, 1.0f); glVertex2f( 30.0f, -30.0f);
+        glTexCoord2f(0.0f, 1.0f); glVertex2f(-30.0f, -30.0f);
+        glTexCoord2f(0.0f, 0.0f); glVertex2f(-30.0f,  30.0f);
+    glEnd();
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glPopMatrix();
+}
+
+void drawGBola(int GBolaTexture)
+{
+    glBindTexture(GL_TEXTURE_2D, GBolaTexture);
+    glBegin(GL_QUADS);
+        glTexCoord2f(1.0f, 0.0f); glVertex2f( 80.0f,  80.0f);
+        glTexCoord2f(1.0f, 1.0f); glVertex2f( 80.0f, -80.0f);
+        glTexCoord2f(0.0f, 1.0f); glVertex2f(-80.0f, -80.0f);
+        glTexCoord2f(0.0f, 0.0f); glVertex2f(-80.0f,  80.0f);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glEnd();
+    glPopMatrix();
+}
+
+void drawSalmonella(int salmonellaTexture)
+{
+    glBindTexture(GL_TEXTURE_2D, salmonellaTexture);
+    glBegin(GL_QUADS);
+        glTexCoord2f(1.0f, 0.0f); glVertex2f( 80.0f,  80.0f);
+        glTexCoord2f(1.0f, 1.0f); glVertex2f( 80.0f, -80.0f);
+        glTexCoord2f(0.0f, 1.0f); glVertex2f(-80.0f, -80.0f);
+        glTexCoord2f(0.0f, 0.0f); glVertex2f(-80.0f,  80.0f);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glEnd();
+    glPopMatrix();
+}
+void drawWave1()
+{
+}
+void drawWave2()
+{
+}
+void drawWave3()
+{
+
+}
+void drawWave4()
+{
+
+}
+void drawWave5()
+{
+
+}
+void drawTheBoss()
+{
+
 }
