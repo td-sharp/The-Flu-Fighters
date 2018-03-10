c60a21ec860d6712a459a64f6eef21de11629204
td-sharp <35699403+td-sharp@users.noreply.github.com>
2018-03-09 17:50:20 -0800

    Update tylerS.cpp

--- tylerS.cpp
+++ tylerS.cpp
@@ -1,6 +1,9 @@
 using namespace std;
 #include "fonts.h"
 #include <GL/glx.h>
+#include <ctime>
+
+extern double timeDiff(struct timespec *start, struct timespec *end);
 
 void startMenu(int xres, int yres, int TitleScreenTexture)
 {
@@ -14,19 +17,24 @@ void startMenu(int xres, int yres, int TitleScreenTexture)
     glBindTexture(GL_TEXTURE_2D, 0);
     glPopMatrix();
 }
-void drawOverlay(int xres, int yres, int lives, int shipTexture)
+void drawOverlay(int xres, int yres, int lives, int overlaidTexture,
+                                                    int shipTexture)
 {
-    /*glPushMatrix();
-    glBindTexture(GL_TEXTURE_2D, overlayTexture);
+    /*glBindTexture(GL_TEXTURE_2D, overlaidTexture);
+    static float angle = 0.0;
+    glRotatef(angle, 0.0f, 0.0f, 1.0f);
+
+    angle = angle + 2.5;
+
     glBegin(GL_QUADS);
-        glTexCoord2f(1.0f, 0.0f); glVertex2f( xres, 200);
+        glTexCoord2f(1.0f, 0.0f); glVertex2f( xres, 60.0f);
         glTexCoord2f(1.0f, 1.0f); glVertex2f( xres, 0.0f);
         glTexCoord2f(0.0f, 1.0f); glVertex2f( 0.0f, 0.0f);
-        glTexCoord2f(0.0f, 0.0f); glVertex2f( 0.0f, 200);
+        glTexCoord2f(0.0f, 0.0f); glVertex2f( 0.0f, 60.0f);
     glEnd();
     glBindTexture(GL_TEXTURE_2D, 0);
     glPopMatrix();
-*/
+    */
     Rect r;
     r.bot = 20;
     r.left = 20;
@@ -35,7 +43,9 @@ void drawOverlay(int xres, int yres, int lives, int shipTexture)
     for (int i = 0; i < lives; i++) {
         glColor3f(1.0f, 1.0f, 1.0f);
         glPushMatrix();
+
         glTranslatef(70*(i+2), 30, 0.0f);
+
         glBindTexture(GL_TEXTURE_2D, shipTexture);
         glBegin(GL_QUADS);
             glTexCoord2f(1.0f, 0.0f); glVertex2f( 10.0f,  10.0f);
@@ -48,11 +58,18 @@ void drawOverlay(int xres, int yres, int lives, int shipTexture)
     }
 }
 
-void drawShip(float posA, float posB, float posC, int shipTexture)
+double drawShip(float posA, float posB, float posC, int shipTexture)
 {
+    static float angle = 0.0;
+    static double thyme = 0.0;
+    struct timespec fthymeStart, fthymeEnd;
+    clock_gettime(CLOCK_REALTIME, &fthymeStart);
     glColor3f(1.0f, 1.0f, 1.0f);
     glPushMatrix();
     glTranslatef(posA, posB, posC);
+    glRotatef(angle, 0.0f, 0.0f, 1.0f);
+    glTranslatef(50, 50, 0);
+    angle = angle + 2.5;
     glBindTexture(GL_TEXTURE_2D, shipTexture);
     //glRotatef(g.ship.angle, 0.0f, 0.0f, 1.0f);
     glBegin(GL_QUADS);
@@ -63,6 +80,38 @@ void drawShip(float posA, float posB, float posC, int shipTexture)
     glEnd();
     glBindTexture(GL_TEXTURE_2D, 0);
     glPopMatrix();
+
+    clock_gettime(CLOCK_REALTIME, &fthymeEnd);
+    thyme += timeDiff(&fthymeStart, &fthymeEnd);
+    return thyme;
+}
+
+double drawShipOPTIMIZED(float posA, float posB, float posC, int shipTexture)
+{
+    //static float move = 0.0;
+    static double thyme = 0.0;
+    struct timespec fthymeStart, fthymeEnd;
+    clock_gettime(CLOCK_REALTIME, &fthymeStart);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glPushMatrix();
+    glTranslatef(posA+60, posB, posC);
+    //glRotatef(angle, 0.0f, 0.0f, 1.0f);
+
+    //angle = angle + 2.5;
+    glBindTexture(GL_TEXTURE_2D, shipTexture);
+    //glRotatef(g.ship.angle, 0.0f, 0.0f, 1.0f);
+    glBegin(GL_QUADS);
+        glTexCoord2f(1.0f, 0.0f); glVertex2f( 30.0f,  30.0f);
+        glTexCoord2f(1.0f, 1.0f); glVertex2f( 30.0f, -30.0f);
+        glTexCoord2f(0.0f, 1.0f); glVertex2f(-30.0f, -30.0f);
+        glTexCoord2f(0.0f, 0.0f); glVertex2f(-30.0f,  30.0f);
+    glEnd();
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glPopMatrix();
+
+    clock_gettime(CLOCK_REALTIME, &fthymeEnd);
+    thyme += timeDiff(&fthymeStart, &fthymeEnd);
+    return thyme;
 }
 
 void drawGBola(int GBolaTexture)
@@ -77,7 +126,7 @@ void drawGBola(int GBolaTexture)
     glEnd();
     glPopMatrix();
 }
-/*
+
 void drawSalmonella(int salmonellaTexture)
 {
     glBindTexture(GL_TEXTURE_2D, salmonellaTexture);
@@ -90,7 +139,6 @@ void drawSalmonella(int salmonellaTexture)
     glEnd();
     glPopMatrix();
 }
-*/
 void drawWave1()
 {
 }
